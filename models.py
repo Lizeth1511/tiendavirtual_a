d6d0f0ba2f4e7814311e56a323d82413a4324203
Lizeth1511 <lizarazonatalynt@gmail.com>
2025-04-21 02:23:36 -0500

    models.py

---
+++ models.py
@@ -0,0 +1,6 @@
+from app import db
+
+class Producto(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    precio = db.Column(db.Float)
